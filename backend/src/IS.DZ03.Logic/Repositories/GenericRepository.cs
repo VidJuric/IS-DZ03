33e6dbff27ca55892ef9184530a334de5af38654
Vid Jurić <juric.vid26@gmail.com>
2022-05-22 10:51:06 +0200

    refactor(): call generic context

--- backend/src/IS.DZ03.Logic/Repositories/GenericRepository.cs
+++ backend/src/IS.DZ03.Logic/Repositories/GenericRepository.cs
@@ -13,55 +13,55 @@ namespace IS.DZ03.Logic.Repositories
 {
     public abstract class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
     {
-        protected readonly AutomobilskeUslugeContext context;
+        protected DbContext DatabaseContext;
         protected readonly ISieveProcessor sieveProcessor;
 
-        public GenericRepository(AutomobilskeUslugeContext context, ISieveProcessor sieveProcessor)
+        public GenericRepository(DbContext databaseContext, ISieveProcessor sieveProcessor)
         {
-            this.context = context;
+            this.DatabaseContext = databaseContext;
             this.sieveProcessor = sieveProcessor;
         }
 
         public void Add(TEntity entity)
         {
-            context.Set<TEntity>().Add(entity);
+            DatabaseContext.Set<TEntity>().Add(entity);
         }
 
         public void AddRange(IEnumerable<TEntity> entities)
         {
-            context.Set<TEntity>().AddRange(entities);
+            DatabaseContext.Set<TEntity>().AddRange(entities);
         }
 
         public async Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> expression)
         {
-            return await context.Set<TEntity>().Where(expression).ToListAsync();
+            return await DatabaseContext.Set<TEntity>().Where(expression).ToListAsync();
         }
 
         public async Task<IEnumerable<TEntity>> GetAll()
         {
-            return await context.Set<TEntity>().ToListAsync();
+            return await DatabaseContext.Set<TEntity>().ToListAsync();
         }
 
         public async Task<IEnumerable<TEntity>> GetAll(SieveModel model)
         {
-            var result = context.Set<TEntity>().AsNoTracking().AsQueryable<TEntity>();
+            var result = DatabaseContext.Set<TEntity>().AsNoTracking().AsQueryable<TEntity>();
             result = sieveProcessor.Apply(model, result);
             return result;
         }
 
         public async Task<TEntity> GetById(int id)
         {
-            return await context.Set<TEntity>().FindAsync(id);
+            return await DatabaseContext.Set<TEntity>().FindAsync(id);
         }
 
         public void Remove(TEntity entity)
         {
-            context.Set<TEntity>().Remove(entity);
+            DatabaseContext.Set<TEntity>().Remove(entity);
         }
 
         public void RemoveRange(IEnumerable<TEntity> entities)
         {
-            context.Set<TEntity>().RemoveRange(entities);
+            DatabaseContext.Set<TEntity>().RemoveRange(entities);
         }
     }
 }
